d97e901a1b4bcb5b848d890225dd8f8251780a91
rss.rodrigo <siga.rodrigo@gmail.com>
2020-08-28 08:11:04 -0300

    aula_js_dio

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function clicou(){
+    alert("Clicou")
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+
+/*function soma(n1, n2){
+    return n1 + n2
+}
+
+
+/*var nome = "Rodrigo Resende";
+var idade = 34;
+alert("Bem Vindo " + nome + " tem " + idade + " anos")
+*/
